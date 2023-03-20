fbdebba1151aa1030e3f58442eb1bd3c1f195583
André Weinfuter <aweinfurter@gmail.com>
2023-03-20 11:27:23 +0044

    inicial

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiply(x, y) {
+    return x * y;
+}
+function greet(name) {
+    return `Olá ${name}`;
+}
+console.log(multiply(10, 20));
+console.log(greet("André"));
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function multiply(x: number, y: number): number {
+    return x * y;
+  }
+  
+function greet(name: string): string {
+    return `Olá ${name}`;
+  }
+
+console.log(multiply(10, 20))
+
+console.log(greet("André"))
